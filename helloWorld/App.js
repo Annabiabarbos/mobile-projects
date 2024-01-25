4508b1f095cb18dc0dd6071cea0e3bf044ab8cec
Anna Beatriz Barbosa Cheliga Horta <52161105850@info.local>
2024-01-25 10:17:05 -0300

    Primeiros elemntos feitos

--- helloWorld/App.js
+++ helloWorld/App.js
@@ -1,20 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, Touchable, View } from 'react-native';
+import { Image, TouchableOpacity } from 'react-native-web';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hello World!</Text>
+      <TextInput
+        style={styles.input}
+        defaultValue='exemplo de input'
+      />
+
+      <Button style = {styles.botao}>
+        <text>Exemplo de botao</text>
+      </Button>
+
+      {/*<Image
+        style={styles.imagem}
+        source={{
+          uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+        }}
+      />*/}
+
       <StatusBar style="auto" />
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#735BA4',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  text: {
+    color: 'white',
+    fontSize: 50,
+    fontWeight: '500'
+  },
+
+  input: {
+    width: '90%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: 'white',
+    padding: 10,
+  },
+
+  imagem: {
+    width: 200,
+    height: 200
+  },
+
+ botao : {
+  
+ }
+
 });
