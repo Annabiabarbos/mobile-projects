ce49afe32308347ce0befde2e4b6849577fbad6c
Anna Beatriz Barbosa Cheliga Horta <52161105850@info.local>
2024-01-25 10:56:48 -0300

    Incio da tela de Login

---
+++ Login/App.js
@@ -0,0 +1,34 @@
+import { StatusBar } from 'expo-status-bar';
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.imagem}
+       source={{
+            uri: 'https://t4.ftcdn.net/jpg/04/28/75/97/360_F_428759715_jsOPITlaytE3QXc2yI1D4U6uwZdVGkRp.jpg',
+          }}
+      />
+      <Text>Login</Text>
+      
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imagem: {
+    height: 100,
+    width: 100,
+  }
+});
+
+
+
