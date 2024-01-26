b852e2ce6a91162eafc8af3060e13920ecf8d1fd
Anna Beatriz Barbosa Cheliga Horta <52161105850@info.local>
2024-01-26 09:48:07 -0300

    Página Login concluída

--- Login/App.js
+++ Login/App.js
@@ -1,17 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Image
         style={styles.imagem}
-       source={{
-            uri: 'https://t4.ftcdn.net/jpg/04/28/75/97/360_F_428759715_jsOPITlaytE3QXc2yI1D4U6uwZdVGkRp.jpg',
-          }}
+        source={{
+          uri: 'https://t4.ftcdn.net/jpg/04/28/75/97/360_F_428759715_jsOPITlaytE3QXc2yI1D4U6uwZdVGkRp.jpg',
+        }}
       />
       <Text>Login</Text>
-      
+
+      <View
+        style={styles.view}
+      >
+
+        <Text
+          style={styles.textEmail}
+        >Email</Text>
+
+        <TextInput
+          style={styles.inputEmail}
+          placeholder='digite seu email'
+        ></TextInput>
+
+        <Text
+          style={styles.textSenha}
+        >Senha</Text>
+
+        <TextInput
+          style={styles.inputSenha}
+          placeholder='digite sua senha'
+          secureTextEntry
+        ></TextInput>
+      </View>
+
+      <TouchableOpacity
+        style={styles.botao}
+      >
+        <Text
+          style={styles.textButton}
+        >Entrar</Text>
+      </TouchableOpacity>
+
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -27,6 +61,54 @@ const styles = StyleSheet.create({
   imagem: {
     height: 100,
     width: 100,
+  },
+
+  view: {
+    width: 300,
+
+  },
+
+  textEmail: {
+    marginTop: 30,
+  },
+
+  inputEmail: {
+    //marginTop: 30,
+
+    width: '90%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: 'blue',
+    borderRadius: 10,
+    padding: 10,
+  },
+
+  textSenha: {
+    marginTop: 30,
+  },
+
+  inputSenha: {
+    width: '90%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: 'blue',
+    borderRadius: 10,
+    padding: 10,
+  },
+
+  botao: {
+    width: '30%',
+    height: 30,
+    marginTop: 30,
+    borderRadius: 10,
+    backgroundColor: '#E5AE2F'
+  },
+
+  textButton: {
+    textAlign: 'center',
+    padding: 5,
+
+    color: 'white'
   }
 });
 
