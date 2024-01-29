0fdcfd6ea88fe50cf8a9964ff5bd56ac26245e9e
Anna Beatriz Barbosa Cheliga Horta <52161105850@info.local>
2024-01-29 11:00:24 -0300

    Conhecendo a biblioteca Styled Componentes

---
+++ styledComponents/App.js
@@ -0,0 +1,87 @@
+import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import styled from "styled-components";
+
+export default function App() {
+  const [count , setCount] = useState(0)
+
+  const increment = () => {
+    setCount (count + 1)
+  }
+
+  const decrement = () => {
+    setCount(count - 1) 
+  }
+
+  //effect 
+
+ useEffect( () => {
+  console.warn(`Contador atualizado : ${count}`)
+ }, [count])
+
+
+  return (
+    <Container style={styles.container}>
+
+      <Text style={styles.contador}>Contador: {count} </Text>
+
+      <TouchableOpacity onPress={increment}
+      style= {styles.btnI}
+      >
+        <Text 
+        style={styles.textBtn}>Incrementar</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={decrement}
+      style= {styles.btnD}
+      >
+       <Text
+        style={styles.textBtn}
+       >Decrementar</Text>
+      </TouchableOpacity>
+
+      <StatusBar style="auto" />
+    </Container>
+  );
+}
+
+const styles = StyleSheet.create({
+  /*container: {
+    flex: 1,
+    backgroundColor: '#DEE0D5',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },*/
+  btnI:{
+    
+    width: '50%',
+    height: 30,
+    marginTop: 50,
+    borderRadius: 10,
+    borderColor : 'white',
+    backgroundColor: '#E2AC48',
+    alignItems: 'center'
+  },
+
+  btnD:{
+    
+    width: '50%',
+    height: 30,
+    marginTop: 50,
+    borderRadius: 10,
+    borderColor: '#fff',
+    backgroundColor: '#983C2D',
+    alignItems: 'center'
+  },
+  
+  textBtn:{
+    color: 'white'
+  },
+
+  contador:{
+    fontSize: 16,
+    //color: 'white'
+  }
+
+});
